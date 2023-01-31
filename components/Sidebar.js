f31c7100755856f99e589c5d0846d14f49906511
joqim <dvetakulajohnbenedict@gmail.com>
2023-01-30 21:50:22 -0800

    styling updates

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -19,10 +19,10 @@ function Sidebar() {
 
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
-      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24" style={{marginTop: "10px"}}>
         <Image src="https://rb.gy/ogau5a" width={30} height={30} />
       </div>
-      <div className="space-y-6 mt-4 mb-2.5 xl:ml-24">
+      <div className="mt-4 mb-2.5 xl:ml-24">
         <SidebarLink text="Home" Icon={HomeIcon} active />
         <SidebarLink text="Explore" Icon={HashtagIcon} />
         <SidebarLink text="Notifications" Icon={BellIcon} />
